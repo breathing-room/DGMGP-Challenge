a53c863afa126d30e1a42a51a0751a1180b9d9cb
Joanna Tomassoni <jctomassoni@gmail.com>
2020-03-30 19:51:46 -0500

    Create tests for App component

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,43 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom'
 import App from './App';
+import Header from './components/Header/Header';
+import Tagline from './components/Tagline/Tagline';
+import Weather from './components/Weather/Weather';
+import Search from './components/Search/Search';
+import Footer from './components/Footer/Footer';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { mount } from 'enzyme'
+
+describe('Main App component', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<App />);
+  })
+  test('renders App component', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('renders Header component', () => {
+    expect(wrapper.contains(<Header />)).toBe(true);
+  })
+
+  test('renders Tagline component', () => {
+    expect(wrapper.contains(<Tagline />)).toBe(true);
+  })
+
+  test('renders Weather component', () => {
+    expect(wrapper.contains(<Weather />)).toBe(true);
+  })
+
+  test('renders Search component', () => {
+    expect(wrapper.contains(<Search />)).toBe(true);
+  })
+
+  test('renders Footer component', () => {
+    expect(wrapper.contains(<Footer />)).toBe(true);
+  })
+})
+
