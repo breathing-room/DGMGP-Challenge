e1209120f73e3aab780a7ee63a9275ae410c3749
Joanna Tomassoni <jctomassoni@gmail.com>
2020-03-31 13:38:43 -0500

    Set up API server and routes

---
+++ api/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 8080;
+
+const getCity = require('./apiCalls/getCity');
+const getWeather = require('./apiCalls/getWeather');
+
+app.get('/', (req, res) => {
+    res.send(path.join(__dirname, 'build/index.html'))
+})
+
+app.get(`/weather`, (req, res) => {
+    console.log(req);
+    const { q } = req.query;
+    getWeather(q).then((response) => {
+        res.send(response.data.list)
+    })
+})
+
+app.get(`/city`, (req, res) => {
+    const { lat, lng } = req.query;
+    getCity(lat, lng).then((response) => {
+        res.send(response.data.results[0].address_components[0].long_name);
+    })
+})
+
+app.listen(port, () => {
+    console.log(`🚀 Server is running on port ${port}`);
+})
