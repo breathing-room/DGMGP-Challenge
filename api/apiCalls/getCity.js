2a3e44d473362330c031fb2fb703ee990739ed6c
Joanna Tomassoni <jctomassoni@gmail.com>
2020-03-31 23:17:51 -0500

    Clean up getCity

--- api/apiCalls/getCity.js
+++ api/apiCalls/getCity.js
@@ -5,13 +5,11 @@ dotenv.config();
 const { GOOGLE_API_KEY } = process.env;
 
 const getCity = (lat, lng) => {
-    return axios({
-        "method":"GET",
-        "url":"https://maps.googleapis.com/maps/api/geocode/json",
-        "params": {
-            "latlng": `${lat},${lng}`,
-            "result_type": "locality",
-            "key": GOOGLE_API_KEY
+    return axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+        params: {
+            latlng: `${lat},${lng}`,
+            result_type: 'locality',
+            key: GOOGLE_API_KEY
             }
         })
 }
