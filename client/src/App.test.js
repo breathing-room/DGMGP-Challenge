09975a4d3bfaab2c158edefea0bcb21b5dda7526
Joanna Tomassoni <jctomassoni@gmail.com>
2020-03-31 21:20:55 -0500

    Clean up App tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App';
 import Header from './components/Header/Header';
-import Tagline from './components/Tagline/Tagline';
 import Weather from './components/Weather/Weather';
 import Search from './components/Search/Search';
 import Footer from './components/Footer/Footer';
@@ -24,20 +23,12 @@ describe('Main App component', () => {
     expect(wrapper.contains(<Header />)).toBe(true);
   })
 
-  test('renders Tagline component', () => {
-    expect(wrapper.contains(<Tagline />)).toBe(true);
-  })
-
   test('renders Weather component', () => {
     expect(wrapper.contains(<Weather />)).toBe(true);
   })
 
   test('renders Search component', () => {
     expect(wrapper.contains(<Search />)).toBe(true);
-  })
-
-  test('renders Footer component', () => {
-    expect(wrapper.contains(<Footer />)).toBe(true);
   })
 })
 
