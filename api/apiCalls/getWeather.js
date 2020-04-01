24730086a5274d2f4deddcb9320a0fcc18118716
Joanna Tomassoni <jctomassoni@gmail.com>
2020-03-31 23:18:18 -0500

    Clean up getWeather

--- api/apiCalls/getWeather.js
+++ api/apiCalls/getWeather.js
@@ -5,18 +5,16 @@ dotenv.config();
 const { WEATHER_API_KEY, WEATHER_API_HOST } = process.env;
 
 const getWeather = (q) => {
-    return axios({
-        "method":"GET",
-        "url":"https://community-open-weather-map.p.rapidapi.com/forecast/daily",
-        "headers": {
-            "content-type":"application/octet-stream",
+    return axios.get('https://community-open-weather-map.p.rapidapi.com/forecast/daily', {
+        headers: {
+            "content-type": 'application/octet-stream',
             "x-rapidapi-host": WEATHER_API_HOST,
             "x-rapidapi-key": WEATHER_API_KEY
         },
-        "params": {
-            "q": q,
-            "cnt":"5",
-            "units":"imperial"
+        params: {
+            q: q,
+            cnt:"5",
+            units:"imperial"
             }
         })
     }
