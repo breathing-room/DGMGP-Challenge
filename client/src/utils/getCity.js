b083f91d969941443797ff053068e804ffa545d6
Joanna Tomassoni <jctomassoni@gmail.com>
2020-04-01 10:25:27 -0500

    Clean up and document utils

--- client/src/utils/getCity.js
+++ client/src/utils/getCity.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
+// Call to our API to retrieve user city
+// from lat/lng retrieved from HTML5 Geolocation API.
+// We need a city name for making call to Open Weather Map API
 const getCityByCoords = (lat, lng) => {
-    return axios.get('/city', {
-        params: {
-            lat,
-            lng
-        }
-    })
+	return axios.get('/city', {
+		params: {
+			lat,
+			lng
+		}
+	})
 }
 
 export default getCityByCoords
--- client/src/utils/getWeather.js
+++ client/src/utils/getWeather.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
+// Call to our API to make the request to Open Weather API
+// to retrieve weather based on city
 const getWeather = (q) => {
-    return axios.get('/weather', {
-        params: {
-            q: q
-        }
-    })
+	return axios.get('/weather', {
+		params: {
+			q: q
+		}
+	})
 }
 
 export default getWeather;
