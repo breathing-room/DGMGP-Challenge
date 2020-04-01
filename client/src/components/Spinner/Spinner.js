df5feb857d4939c5f22a0d4a63dcd2d6f8846653
Joanna Tomassoni <jctomassoni@gmail.com>
2020-03-31 19:42:46 -0500

    Change spinner color

--- client/src/components/Spinner/Spinner.js
+++ client/src/components/Spinner/Spinner.js
@@ -4,7 +4,7 @@ import Loader from 'react-loader-spinner';
 class Spinner extends React.Component {
    render() {
     return(
-        <Loader type="BallTriangle" color="#f4f4f4" height={80} width={80} style={{padding: '40px'}}/>
+        <Loader type="BallTriangle" color="rgba(3, 190, 190, 0.651)" height={80} width={80} style={{padding: '40px'}}/>
     );
    }
 }
